e614e8e8c6f02724f8b531a0e4cab1848e869d78
Hernan Schmidt <lagartoflojo@users.noreply.github.com>
2019-11-25 14:15:58 +0100

    Small refactoring

--- spec/ragmob/parsers/api_information_spec.rb
+++ spec/ragmob/parsers/api_information_spec.rb
@@ -5,10 +5,11 @@ describe Ragmob::Parsers::ApiInformation do
 
   it "parses the XML correctly" do
     api_information = Ragmob::Parsers::ApiInformation.parse(xml, single: true)
+
     _(api_information.api_version).must_equal '1.19.0'
     _(api_information.company_name).must_equal 'supportsuse'
     _(api_information.timestamp).must_equal Time.at(1_574_638_362)
-    {
+    _(api_information.permissions).must_equal(
       "perm_command" => "full_access",
       "perm_reporting_support" => true,
       "perm_reporting_presentation" => true,
@@ -18,9 +19,6 @@ describe Ragmob::Parsers::ApiInformation do
       "perm_ecm" => false,
       "perm_backup" => false,
       "perm_scim" => false
-    }.each_pair do |name, value|
-      _(api_information.permissions).must_include name
-      _(api_information.permissions[name]).must_equal value
-    end
+    )
   end
 end
