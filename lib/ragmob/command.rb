8b083e0128760539f5271f47be63e640434fe535
Hernán Schmidt <lagartoflojo@users.noreply.github.com>
2019-11-25 22:38:38 +0100

    Add Client#check_health

--- lib/ragmob/command.rb
+++ lib/ragmob/command.rb
@@ -1,10 +1,16 @@
 require "ragmob/parsers/api_information"
+require "ragmob/parsers/health_check"
 
 module Ragmob
   module Command
     def get_api_info
       resp = connection.get('api/command?action=get_api_info')
       Ragmob::Parsers::ApiInformation.parse(resp.body.to_s, single: true)
+    end
+
+    def check_health
+      resp = connection.get('api/command?action=check_health')
+      Ragmob::Parsers::HealthCheck.parse(resp.body.to_s, single: true)
     end
   end
 end
